c014a4e8bcd21a8a96369c7868c639345927bad4
spbhanda <spbhanda@gmail.com>
2022-08-01 22:08:53 -0500

    wokring through sql data

--- server.js
+++ server.js
@@ -21,10 +21,43 @@ const db = mysql.createConnection(
 );
 
 // Check the connection, delete it later
-app.get("/", (req, res) => {
-   res.json({
-      messagge: "Hello World",
-   });
+// app.get("/", (req, res) => {
+//    res.json({
+//       messagge: "Hello World",
+//    });
+// });
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//    console.log(rows);
+// });
+//  **************************************************************************
+
+// db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
+//    if (err) {
+//       console.log(err);
+//    }
+//    console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
+//    if (err) {
+//       console.log(err);
+//    }
+//    console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES(?,?,?,?)`;
+
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+   if (err) {
+      console.log("Error: " + err);
+   }
+   console.log(result);
 });
 
 // Default response for any other request (NOT FOUND)
