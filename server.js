ae13f45399af5c797ed214cbb238913ad08cc7cf
spbhanda <spbhanda@gmail.com>
2022-08-06 10:41:19 -0500

    create/post new candidate

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require("./utils/inputCheck");
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -71,6 +72,30 @@ app.delete("/api/candidate/:id", (req, res) => {
             id: req.params.id,
          });
       }
+   });
+});
+
+// Create a candidate
+// Create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+   const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+   if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+   }
+   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+   const params = [body.first_name, body.last_name, body.industry_connected];
+
+   db.query(sql, params, (err, result) => {
+      if (err) {
+         res.status(400).json({ error: err.message });
+         return;
+      }
+      res.json({
+         message: "success",
+         data: body,
+      });
    });
 });
 
